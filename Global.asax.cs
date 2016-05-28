4279897411a738a9a977ce81bacea721bba8e46c
Ensar Topal <ensartopal34@gmail.com>
2016-05-28 06:58:15 +0000

    others

---
+++ Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using BlogMVC.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace BlogMVC
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Database.SetInitializer(new BlogInıtializer());
+                             
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
